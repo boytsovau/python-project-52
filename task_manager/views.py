ba2fcbbd729cea87ac15fe707eed6cc503098559
boytsovau <alexbonch@gmail.com>
2024-02-22 15:58:08 +0300

    modified:   task_manager/views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.utils.translation import gettext_lazy as _
