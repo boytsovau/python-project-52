375f9a313b55c2bf09320df0626d079c87d4dbd0
boytsovau <alexbonch@gmail.com>
2024-02-22 16:15:31 +0300

    modified:   task_manager/users/models.py

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class TaskUser(User):
 
     def __str__(self):
-        return " ".join((str(self.first_name), str(self.last_name)))
+        return self.get_full_name() or self.username
