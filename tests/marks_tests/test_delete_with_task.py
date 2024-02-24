d1542febff0884d35fab5bcfb742115ddc1578f0
boytsovau <alexbonch@gmail.com>
2024-02-24 14:50:08 +0300

    modified:   tests/marks_tests/test_delete_with_task.py 	modified:   tests/status_tests/test_delete_with_task.py

--- tests/marks_tests/test_delete_with_task.py
+++ tests/marks_tests/test_delete_with_task.py
@@ -2,6 +2,7 @@ from task_manager.users.models import TaskUser as User
 from django.urls import reverse_lazy as reverse
 from django.test import TransactionTestCase
 from task_manager.mark.models import Mark
+from django.utils.translation import gettext as _
 from tests import FIXTURE_DIR
 
 
@@ -13,6 +14,8 @@ class DeleteMarkWithTask(TransactionTestCase):
         user = User.objects.all().first()
         self.assertEqual(Mark.objects.all().count(), 1)
         self.client.force_login(user=user)
-        self.client.get(reverse('mark_delete',
-                                kwargs={'pk': mark.id}))
+        response = self.client.get(reverse('mark_delete',
+                                   kwargs={'pk': mark.id}))
         self.assertEqual(Mark.objects.all().count(), 1)
+        expected_message = _("Are you sure delete")
+        self.assertContains(response, expected_message)
--- tests/status_tests/test_delete_with_task.py
+++ tests/status_tests/test_delete_with_task.py
@@ -2,6 +2,7 @@ from task_manager.users.models import TaskUser as User
 from django.urls import reverse_lazy as reverse
 from django.test import TransactionTestCase
 from task_manager.status.models import Status
+from django.utils.translation import gettext as _
 from tests import FIXTURE_DIR
 
 
@@ -13,6 +14,8 @@ class DeleteStutusWithTask(TransactionTestCase):
         user = User.objects.all().first()
         self.assertEqual(Status.objects.all().count(), 1)
         self.client.force_login(user=user)
-        self.client.get(reverse('status_delete',
-                                kwargs={'pk': status.id}))
+        response = self.client.get(reverse('status_delete',
+                                   kwargs={'pk': status.id}))
         self.assertEqual(Status.objects.all().count(), 1)
+        expected_message = _("Are you sure delete")
+        self.assertContains(response, expected_message)
